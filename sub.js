5f223e0d12cf095507ba490345acea2d7f712ea5
V.Pardha Saradhi <paradhasaradi@gmail.com>
2022-04-14 09:58:55 +0530

    added

--- sub.js
+++ sub.js
@@ -1,8 +1,8 @@
-file name - substraction
+
 function sub() {
 var a = document.getElementById("firstno").value
 var b = document.getElementById("secondno").value
 var result
 result = a - b
-document.getElementById("result").value
+document.getElementById("result").value=result
 }
